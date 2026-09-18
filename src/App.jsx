d09ea9bf19606eae52536a85c0d9e1db63896bf2
Mary Toledo <mftoledo2024@hccci.edu.ph>
2026-09-18 11:33:09 +0800

    Task 2: add /countries/:countryCode route

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { Routes, Route } from "react-router";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
+import CountryDetailPage from "./pages/CountryDetailPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
-
-
-  const App = () => {
-    return (
-      <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<HomePage />} />
-      <Route path="countries" element={<CountriesPage />} />
-      <Route path="bucket-list" element={<BucketListPage />} />
-      <Route path="about" element={<AboutPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+const App = () => {
+  return (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+    <Route index element={<HomePage />} />
+    <Route path="countries">
+      <Route index element={<CountriesPage />} />
+      <Route path=":countryCode" element={<CountryDetailPage />} />
       </Route>
-      </Routes>
-     );
-   };
-   export default App;
+    <Route path="bucket-list" element={<BucketListPage />} />
+    <Route path="about" element={<AboutPage />} />
+    <Route path="*" element={<NotFoundPage />} />
+    </Route>
+    </Routes>
+    );
+  };
+  export default App;
